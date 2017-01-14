8d91ead695a286873e3903c01093c8d76325a71e
mariomoureperez <mariomoureperez@gmail.com>
2017-01-14 12:02:09 +0100

    terminado

--- app/src/main/java/com/example/mario/comunicacionfragments/Main2ActivityFragment.java
+++ app/src/main/java/com/example/mario/comunicacionfragments/Main2ActivityFragment.java
@@ -1,10 +1,15 @@
 package com.example.mario.comunicacionfragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,35 @@ public class Main2ActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main2, container, false);
+        View layFrag2=inflater.inflate(R.layout.fragment_main2, container, false);
+
+        TextView text2= (TextView) layFrag2.findViewById(R.id.text2);
+
+        String str=getActivity().getIntent().getStringExtra("llamar");
+
+        text2.setText(str);
+
+        Button button2=(Button) layFrag2.findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent databack=new Intent();
+                databack.putExtra("llamar2","mensaje desde fragment2");
+
+
+
+
+                setResult(RESULT_OK,databack);
+                finish();
+
+            }
+
+
+        });
+
+        return layFrag2;
     }
+
+
 }
