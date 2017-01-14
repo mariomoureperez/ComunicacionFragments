3869a1171e189445b566092c83ee525dc437cba8
mariomoureperez <mariomoureperez@gmail.com>
2017-01-14 12:00:45 +0100

    introducido starActivityforResult en mainActivityFragment

--- app/src/main/java/com/example/mario/comunicacionfragments/MainActivityFragment.java
+++ app/src/main/java/com/example/mario/comunicacionfragments/MainActivityFragment.java
@@ -1,22 +1,59 @@
 package com.example.mario.comunicacionfragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    public final static int CODIGO=1;
+    private TextView text;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View layFragment=inflater.inflate(R.layout.fragment_main, container, false);
+        Button button=(Button) layFragment.findViewById(R.id.button);
+        text=(TextView)layFragment.findViewById(R.id.text);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(),Main2Activity.class);
+                intent.putExtra("llamar","mensaje desde fragment");
+                startActivityForResult(intent,CODIGO);
+
+            }
+
+
+        });
+
+
+
+        return layFragment;
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == CODIGO){
+            if(resultCode == RESULT_OK){
+                String result=data.getStringExtra("llamar2");
+                text.setText(result);
+            }
+        }
     }
 }
